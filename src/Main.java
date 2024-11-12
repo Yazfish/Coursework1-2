48a6afc17a4932c61ac9ecb76bb407f531ef5a25
Yazfish <sergei_kuzmin@mail.ru>
2024-11-13 02:25:15 +0300

    Start commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Курсовая работа 1, Введение в профессию и синтаксис языка.
+// Кузьмин Сергей
+// Java-разработчик IND 36.0
+//
+public class Main {
+    public static void main(String[] args) {
+    }
+}
